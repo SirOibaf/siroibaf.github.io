11a9499dfdb37f5b7d1e6c8cf08bbf73492f9d0f
SirOibaf <garbage.fabio@gmail.com>
2014-11-22 19:50:21 +0100

    progress

--- js.js
+++ js.js
@@ -1,5 +1,32 @@
 /* Javascript index.html */
 
+// Set the sizes of the blocks
+function setSizes(){
+    var width = $(window).width();
+    var height = $(window).height();
+
+    if (width < 800){
+        width = 800;
+    }
+
+    if (height < 600){
+        hegith = 600;
+    }
+    
+    // set width all children of body
+    $('body').css('width', width);
+    
+    $('#name').css('width', width-40); //width - margin left
+    $('#shbio').css('width', width-40); //width - margin left
+};
+
 $(document).ready(function() {
-    alert("motherfucker!!!");
+    setSizes();
 });
+
+$(window).resize(function() {
+    // Action to do when window is resized
+   setSizes();
+});
+
+
