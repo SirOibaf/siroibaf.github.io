11e1ef0526a8169d965322d58c363f77036a5d1d
SirOibaf <buso.fabio@gmail.com>
2015-07-28 23:19:38 +0200

    fix layout issues mobile

--- js.js
+++ js.js
@@ -2,6 +2,8 @@
 
 // Set the sizes of the blocks
 function setSizesDesktop(){
+
+  $("*").removeAttr("style");
   // show menu
   $("#menu").css("visibility", "visible");
 
@@ -17,18 +19,18 @@ function setSizesDesktop(){
   }
 
   // set width of all the boxes
-  $('#top').width(width);
-  $('#title').width(width);
-  $('#bgmiddle').width(width);
-  $('#bottom').width(width);
+  $('#top').css("width", width);
+  $('#title').css("width", width);
+  $('#bgmiddle').css("width", width);
+  $('#bottom').css("width", width);
 
   // set height of all the boxes
-  $('#top').height(height*1/100);
-  $('#title').height(height*17/100);
-  $('#titleInside').height(height*17/100);
-  $('#bgmiddle').height(height*67/100);
-  $('#bottom').height(height*15/100);
-  $('#insideBottom').height(height*15/100);
+  $('#top').css("height", height*1/100);
+  $('#title').css("height", height*17/100);
+  $('#titleInside').css("height", height*17/100);
+  $('#bgmiddle').css("height", height*67/100);
+  $('#bottom').css("height",height*15/100);
+  $('#insideBottom').css("height", height*15/100);
 
   // Se Maometto non va alla montagna...
   $("#bioBox").css("height", "100%");
@@ -44,7 +46,7 @@ function setSizesDesktop(){
   $("#inside").css("top", ($("#me").parent().height() - 370)/2 + "px"); //Same top of the image
 
   // Bottom settings
-  $(".floater").height(($("#bottom").height()-65)/2);
+  $(".floater").css("height", ($("#bottom").height()-65)/2);
 
   // Button behavior
   $("#projectsLink").click(function() {
@@ -61,7 +63,32 @@ function setSizesDesktop(){
 };
 
 function setSizesMobile() {
+  // reset previous options
+  $("*").removeAttr("style");
   $("#menu").css("visibility", "hidden");
+  $(".floater").css("visibility", "hidden");
+
+  var white = $(window).height() - ($("#top").height() + $("#title").height() + $("#bgmiddle").height() + $("#bottom").height());
+
+  if (white > 0){
+    $("#left").css("padding-top", white/6);
+    $("#left").css("height", white/3 + 208);
+    $("#me").css("height", white/3 + 200);
+    $("#right").css("margin-top", white/4);
+    $("#right").css("padding-bottom", white/4 + 15);
+  }
+
+  $("#right").on("swipeleft", function() {
+    $("#text").text("I haven't done any interesting project yet");
+  });
+
+  $("#right").on("swiperight", function() {
+    $("#text").html("I'm a 22 years old nerd, passionate about technology and open source.<br>" +
+        "I received my bachelor degree in Computer Science at the University of Trento. <br>" +
+        "Now I am looking forward to start a new exiting experience as student of the EIT ICT Master School" +
+        "in the major of Cloud Computing and Services.<br>" +
+        "When not studying, I try to contribute to some opensource projects or I read books.");
+  });
 }
 
 $(document).ready(function() {
@@ -73,14 +100,14 @@ $(document).ready(function() {
 });
 
 $(window).resize(function() {
-  $("body").css("overflow", "hidden");
 
   if ($(window).width() >= 1024) {
+    $("body").css("overflow", "hidden");
     setSizesDesktop();
+    $("body").css("overflow", "auto");
   } else {
     setSizesMobile();
   }
 
-  $("body").css("overflow", "auto");
 });
 
