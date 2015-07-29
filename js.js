4977cd60d45f7db79926dbe76e9527dcf9e018a9
SirOibaf <buso.fabio@gmail.com>
2015-07-29 23:40:30 +0200

    fix mobile layout issues

--- js.js
+++ js.js
@@ -4,8 +4,12 @@
 function setSizesDesktop(){
 
   $("*").removeAttr("style");
+  $("body").css("overflow", "hidden");
+
   // show menu
   $("#menu").css("visibility", "visible");
+  // Disable arrow
+  $(".arrow").hide();
 
   var width = $(window).width();
   var height = $(window).height();
@@ -56,38 +60,64 @@ function setSizesDesktop(){
   $("#bioLink").click(function() {
     $("#text").html("I'm a 22 years old nerd, passionate about technology and open source.<br>" +
         "I received my bachelor degree in Computer Science at the University of Trento. <br>" +
-        "Now I am looking forward to start a new exiting experience as student of the EIT ICT Master School" +
+        "Now I am looking forward to start a new exiting experience as student of the EIT ICT Master School " +
         "in the major of Cloud Computing and Services.<br>" +
         "When not studying, I try to contribute to some opensource projects or I read books.");
   });
+
+
+  $("body").css("overflow", "auto");
 };
 
 function setSizesMobile() {
   // reset previous options
+
+  var aRight = $("#ar").css("visibility");
+  var aLeft = $("#al").css("visibility");
+
   $("*").removeAttr("style");
   $("#menu").css("visibility", "hidden");
   $(".floater").css("visibility", "hidden");
+  $(".arrow").show();
 
   var white = $(window).height() - ($("#top").height() + $("#title").height() + $("#bgmiddle").height() + $("#bottom").height());
 
   if (white > 0){
-    $("#left").css("padding-top", white/6);
-    $("#left").css("height", white/3 + 208);
-    $("#me").css("height", white/3 + 200);
-    $("#right").css("margin-top", white/4);
-    $("#right").css("padding-bottom", white/4 + 15);
+    // reset heights
+    var height = $(window).height();
+    $('#top').css("height", height*1/100);
+    $('#title').css("height", height*7/100);
+    $('#bgmiddle').css("height", height*62.5/100 + 1);
+    $('#left').css("height", $("#bgmiddle").parent().height()*26/100);
+    $('#right').css("height", $("#bgmiddle").parent().height()*30/100);
+    $('#bottom').css("height",height*27/100);
+
+    $('#me').css("height", $("#bgmiddle").height()*38/100);
+    $('#me').css("top", $("#me").parent().height()*5/100);
+
+    $("#leftArrow, #rightArrow").css("height", $("#right").height());
   }
 
-  $("#right").on("swipeleft", function() {
-    $("#text").text("I haven't done any interesting project yet");
+  if ((aLeft == "visible" && aRight == "visible") || (aLeft == "hidden")) {
+    $("#al").css("visibility", "hidden");
+  } else if (aRight == "hidden") {
+    $("#ar").css("visibility", "hidden");
+  }
+
+  $("#ar").click(function() {
+    $("#text").html("I haven't done any interesting project yet<br><br> <br><br><br><br>");
+    $("#ar").css("visibility", "hidden");
+    $("#al").css("visibility", "visible");
   });
 
-  $("#right").on("swiperight", function() {
+  $("#al").click(function() {
     $("#text").html("I'm a 22 years old nerd, passionate about technology and open source.<br>" +
         "I received my bachelor degree in Computer Science at the University of Trento. <br>" +
-        "Now I am looking forward to start a new exiting experience as student of the EIT ICT Master School" +
+        "Now I am looking forward to start a new exiting experience as student of the EIT ICT Master School " +
         "in the major of Cloud Computing and Services.<br>" +
         "When not studying, I try to contribute to some opensource projects or I read books.");
+    $("#al").css("visibility", "hidden");
+    $("#ar").css("visibility", "visible");
   });
 }
 
@@ -102,9 +132,7 @@ $(document).ready(function() {
 $(window).resize(function() {
 
   if ($(window).width() >= 1024) {
-    $("body").css("overflow", "hidden");
     setSizesDesktop();
-    $("body").css("overflow", "auto");
   } else {
     setSizesMobile();
   }
