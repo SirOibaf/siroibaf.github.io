284f4ce0fb117ad8af01eca7f077962734028a26
SirOibaf <buso.fabio@gmail.com>
2015-08-06 18:42:36 +0200

    fix layout bugs in mobile version

--- js.js
+++ js.js
@@ -1,5 +1,12 @@
 /* Javascript index.html */
 
+var page = 0;
+
+function whiteBottom() {
+  return $("#top").height() + $("#title").height() + $("#bgmiddle").height() + $("#bottom").height() < $(window).height();
+}
+
+
 // Set the sizes of the blocks
 function setSizesDesktop(){
 
@@ -53,62 +60,53 @@ function setSizesDesktop(){
   $(".floater").css("height", ($("#bottom").height()-65)/2);
 
   // Button behavior
-  $("#projectsLink").click(function() {
-    $("#text").text("I haven't done any interesting project yet");
-  });
-
-  $("#bioLink").click(function() {
-    $("#text").html("I'm a 22 years old nerd, passionate about technology and open source.<br>" +
-        "I received my bachelor degree in Computer Science at the University of Trento. <br>" +
-        "Now I am looking forward to start a new exciting experience as student of the EIT ICT Master School " +
-        "in the major of Cloud Computing and Services.<br>" +
-        "When not studying, I try to contribute to some opensource projects or I read books.");
-  });
-
-
-  $("body").css("overflow", "auto");
+   $("body").css("overflow", "auto");
 };
 
 function setSizesMobile() {
-  alert("Function called");
-  // reset previous options
-
   var aRight = $("#ar").css("visibility");
   var aLeft = $("#al").css("visibility");
 
   $("*").removeAttr("style");
+
+  //hide desktop features
   $("#menu").css("visibility", "hidden");
   $(".floater").css("visibility", "hidden");
   $(".arrow").show();
 
-  var white = $(window).height() - ($("#top").height() + $("#title").height() + $("#bgmiddle").height() + $("#bottom").height());
-
-  alert(white);
-  if (white > 0){
-    //set containers' height
+  var mobile = navigator.userAgent.match(/Mobi/) || navigator.userAgent.match(/Android/);
+  var wb = whiteBottom();
+  if ((mobile && ($(window).height() > $(window).width())) || wb) {
+    // mobile!
     var height = $(window).height();
-    $("body").css("font-size", "2em");
+    console.log(height);
+
     $('#top').css("height", height*1/100);
     $('#title').css("height", height*7/100);
-    $('#bgmiddle').css("height", height*65.5/100 + 1);
+    $('#bgmiddle').css("height", height*65.5/100);
     $('#left').css("height", $("#bgmiddle").parent().height()*26/100);
     $('#right').css("height", $("#bgmiddle").parent().height()*30/100);
-    $('#bottom').css("height",height*26/100);
+    $('#right').css("padding-top", $("#right").height()*15/100);
+    $('#bottom').css("height",height*25.5/100);
 
-    $('#me').css("height", $("#bgmiddle").height()*45/100);
+    // img size
+    $('#me').height($("#bgmiddle").height()*40/100);
     $('#me').css("top", $("#me").parent().height()*15/100);
 
     // set description font-size
-   // $("#right").css("font-size", $("#right").height() * 0.10
+    // $("#right").css("font-size", $("#right").height() * 0.10
 
-    $("#leftArrow, #rightArrow").css("height", $("#right").height());
-    $("#facebook .floater").height($("#bottom").height()*13/100);
+    if (!wb || mobile) {
+      $("#facebook .floater").height($("#bottom").height()*13/100);
+    }
   }
 
+  $("#leftArrow, #rightArrow").css("height", $("#right").height());
 
-  if ((aLeft == "visible" && aRight == "visible") || (aLeft == "hidden")) {
+
+  if (page == 0) {
     $("#al").css("visibility", "hidden");
-  } else if (aRight == "hidden") {
+  } else {
     $("#ar").css("visibility", "hidden");
   }
 
@@ -122,32 +120,52 @@ $(document).ready(function() {
     setSizesMobile();
   }
 
+  $("#projectsLink").click(function() {
+    $("#text").text("I haven't done any interesting project yet");
+    page = 1;
+  });
+
+  $("#bioLink").click(function() {
+    $("#text").html("I'm a 22 years old nerd, passionate about technology and open source.<br>" +
+        "I received my bachelor degree in Computer Science at the University of Trento. <br>" +
+        "Now I am looking forward to start a new exciting experience as student of the EIT ICT Master School " +
+        "in the major of Cloud Computing and Services.<br>" +
+        "When not studying, I try to contribute to some opensource projects or I read books.");
+    page = 0;
+  });
+
   $("#ar").click(function() {
-        $("#text").html("I haven't done any interesting project yet<br><br><br>");
-        $("#ar").css("visibility", "hidden");
-        $("#al").css("visibility", "visible");
-        setSizesMobile();
-      });
+    $("#text").html("I haven't done any interesting project yet<br><br><br><br><br><br><br>");
+    $("#ar").css("visibility", "hidden");
+    $("#al").css("visibility", "visible");
+    page = 1;
+    setSizesMobile();
+  });
 
   $("#al").click(function() {
-        $("#text").html("I'm a 22 years old nerd, passionate about technology and open source.<br>" +
-            "I received my bachelor degree in Computer Science at the University of Trento. <br>" +
-            "Now I am looking forward to start a new exciting experience as student of the EIT ICT Master School " +
-            "in the major of Cloud Computing and Services.<br>" +
-            "When not studying, I try to contribute to some opensource projects or I read books.");
-        $("#al").css("visibility", "hidden");
-        $("#ar").css("visibility", "visible");
-        setSizesMobile();
+    $("#text").html("I'm a 22 years old nerd, passionate about technology and open source.<br>" +
+        "I received my bachelor degree in Computer Science at the University of Trento. <br>" +
+        "Now I am looking forward to start a new exciting experience as student of the EIT ICT Master School " +
+        "in the major of Cloud Computing and Services.<br>" +
+        "When not studying, I try to contribute to some opensource projects or I read books.");
+    $("#al").css("visibility", "hidden");
+    $("#ar").css("visibility", "visible");
+    page = 0;
+    setSizesMobile();
   });
 });
 
 $(window).resize(function() {
-
   if ($(window).width() >= 1024 && window.screen.width >= 1024) {
-    $("#stylesheet").attr('href', "desktop.css");
+    if ($("#stylesheet").attr('href') != "dekstop.css") {
+      $("#stylesheet").attr('href', "desktop.css");
+    }
     setSizesDesktop();
   } else {
-    $("#stylesheet").attr('href', "mobile.css");
+    if ($("#stylesheet").attr('href') != "mobile.css") {
+      $("#stylesheet").attr('href', "mobile.css");
+    }
     setSizesMobile();
   }
 });
+
